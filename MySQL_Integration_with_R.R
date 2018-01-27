1686aceb3829321287ceac44ddfa6dc0388f88ff
Shingare Shubbham <shingareshubham@gmail.com>
2018-01-28 00:21:15 +0530

    Create MySQL_Integration_with_R.R

---
+++ MySQL_Integration_with_R.R
@@ -0,0 +1,27 @@
+# Set working Directory
+setwd("C:/R/sql")
+
+# install.packages("RMySQL","dbConnect")
+library(RMySQL)
+library(dbConnect)
+
+# Create Connection between MySQL and R
+# MySQL()    => DBIDriver            # user       => user name
+# password   => user password        # dbname     => DataBase name
+con <- dbConnect(MySQL(),user="user_name", password="user_password",dbname="database_name", host="localhost")
+
+# shows list of tables in database
+dbListTables(con)
+
+# Write SQL Query
+myQuery <- "select * from table_name;"
+
+# Store a result in variable
+df <- dbGetQuery(con,myQuery)
+
+# Now you have all value
+# Perform your R functions
+
+str(df)
+head(df)
+summary(df)
